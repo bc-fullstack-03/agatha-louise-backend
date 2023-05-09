a1c6b726c702dfc9c90635eb60a166e933b5c472
Agatha Louise <86273896+agathalouise@users.noreply.github.com>
2023-05-09 00:33:53 -0300

    excluindo imports não utilizados e organização das pastas

--- src/main/java/com/sysmap/parrot/entities/user/model/ChangePasswordUserRequest.java
+++ src/main/java/com/sysmap/parrot/entities/user/model/ChangePasswordUserRequest.java
@@ -1,4 +1,4 @@
-package com.sysmap.parrot.entities.user.dto;
+package com.sysmap.parrot.entities.user.model;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
--- src/main/java/com/sysmap/parrot/entities/user/model/CreateUserRequest.java
+++ src/main/java/com/sysmap/parrot/entities/user/model/CreateUserRequest.java
@@ -1,4 +1,4 @@
-package com.sysmap.parrot.entities.user.dto;
+package com.sysmap.parrot.entities.user.model;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
