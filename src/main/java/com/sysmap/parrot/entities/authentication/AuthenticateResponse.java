1499eaa443b0c0dd9856480d93042c6748ae7670
Agatha Louise <86273896+agathalouise@users.noreply.github.com>
2023-05-09 08:21:30 -0300

    fake commit to test

--- src/main/java/com/sysmap/parrot/entities/authentication/AuthenticateResponse.java
+++ src/main/java/com/sysmap/parrot/entities/authentication/AuthenticateResponse.java
@@ -3,12 +3,14 @@ package com.sysmap.parrot.entities.authentication;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.UUID;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@ToString
 public class AuthenticateResponse {
 
 	private UUID id;
